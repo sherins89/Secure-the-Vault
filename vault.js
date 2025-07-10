3a778765ee2b7f76b90fdc7ac96fee6f0d5170d4
Sherin <sherin.daniel89@yahoo.com>
2025-07-09 21:06:03 -0400

    js1

---
+++ vault.js
@@ -0,0 +1,14 @@
+let code1;
+code1 = 7 + 3;
+
+let code2;
+code2 = (82 - 2) / 2
+
+let code3;
+code3 = 44 - (40% 7);
+
+let message = "The vault has been secured. The combination is";
+
+
+let codeA; code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
