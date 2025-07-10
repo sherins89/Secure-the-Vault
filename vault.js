625a0eb0721c26be8eeac7cf09eac4725e41aa3f
Sherin <sherin.daniel89@yahoo.com>
2025-07-09 21:28:55 -0400

    j3

--- vault.js
+++ vault.js
@@ -12,3 +12,8 @@ let message = "The vault has been secured. The combination is";
 
 let codeA; code1 + "-" + code2 + "-" + code3;
 let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
+
